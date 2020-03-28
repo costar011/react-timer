1cefb30d888fc5f44c9591c64aec4db1baed2c6d
costar011 <51503128+costar011@users.noreply.github.com>
2020-03-28 14:49:59 +0900

    timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let autoTime = null;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timeSecond: 0,
+      isStart: false
+    };
+  }
+  render() {
+    const { timeSecond } = this.state;
+    return (
+      <div className="timerBox">
+        <div className="timerBox__time">
+          <span>{timeSecond}</span>
+        </div>
+        <div className="timerBox__record"></div>
+        <div className="timerBox__btn">
+          <input type="button" value="START" onClick={this._startTimer}></input>
+          <input type="button" value="STOP" onClick={this._stopTimer}></input>
+          <input type="button" value="RECODE"></input>
+        </div>
+      </div>
+    );
+  }
+
+  // setInterval : 1초마다 무한재생이고 state값이 바뀐다.
+  _startTimer = () => {
+    const { isStart } = this.state;
+    if (!isStart) {
+      this.setState({
+        isStart: true
+      });
+
+      autoTime = setInterval(() => {
+        this.setState({
+          timeSecond: this.state.timeSecond + 1
+        });
+      }, 1000);
+    } else {
+      alert("이미 타이머가 작동중");
+    }
+  };
+
+  _stopTimer = () => {
+    this.setState({
+      isStart: false
+    });
+
+    clearInterval(autoTime);
+    this.setState({
+      timeSecond: 0
+    });
+  };
 }
-
 export default App;
