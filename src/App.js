23ee5d0f34cfd15e017d77cf7b7dc4833de914ad
costar011 <51503128+costar011@users.noreply.github.com>
2020-03-28 15:17:54 +0900

    complete timer

--- src/App.js
+++ src/App.js
@@ -9,21 +9,30 @@ class App extends React.Component {
 
     this.state = {
       timeSecond: 0,
-      isStart: false
+      isStart: false,
+      records: []
     };
   }
   render() {
-    const { timeSecond } = this.state;
+    const { timeSecond, records } = this.state;
     return (
       <div className="timerBox">
         <div className="timerBox__time">
           <span>{timeSecond}</span>
         </div>
-        <div className="timerBox__record"></div>
+        {/*map : 반복*/}
+        <div className="timerBox__record">
+          <ul>
+            {records.map((re, index) => {
+              return <li key={index}>{re}</li>;
+            })}
+          </ul>
+        </div>
         <div className="timerBox__btn">
           <input type="button" value="START" onClick={this._startTimer}></input>
           <input type="button" value="STOP" onClick={this._stopTimer}></input>
-          <input type="button" value="RECODE"></input>
+          <input type="button" value="RECODE" onClick={this._record}></input>
+          <input type="button" value="INIT" onClick={this._recordInit}></input>
         </div>
       </div>
     );
@@ -55,6 +64,22 @@ class App extends React.Component {
     clearInterval(autoTime);
     this.setState({
       timeSecond: 0
+    });
+  };
+
+  _record = () => {
+    const { isStart, records } = this.state;
+
+    if (isStart) {
+      records.push(this.state.timeSecond);
+    } else {
+      return;
+    }
+  };
+
+  _recordInit = () => {
+    this.setState({
+      records: []
     });
   };
 }
